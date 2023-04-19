c66d64d254927252dd4690b63bc50f602f62fbd1
mehpatel79 <123248785+mehpatel79@users.noreply.github.com>
2023-04-19 18:49:34 +0530

    Parent Child Object Changes

--- Accordion.jsx
+++ Accordion.jsx
@@ -1,11 +1,15 @@
 import React, { useState } from "react";
 import Panel from "./Panel";
-// import PanelA from './PanelA';
-// import PanelB from './PanelB';
-// import PanelC from './PanelC';
 
 const Accordion = () => {
     const [activePanel, setActivePanel] = useState(0);
+    const [panelData, setPanelData] = useState([
+        {id:1, value:"panel1"},
+        {id:2, value:"panel2"},
+        {id:3, value:"panel3"}
+    ]);
+
+    
     
     const showPanel = (panel) => {
         if(activePanel !== panel){
@@ -13,41 +17,23 @@ const Accordion = () => {
             }
         else {
             setActivePanel(0);
-            };
+            }
 
-    };
+    }
     
     
 
 
     return (<>
             <h2 style={{color:"purple"}}><center>City : <i><b>"VADODARA"</b></i> </center></h2>
-            
-            <div style={{border: "1px solid black", padding:"5px", margin:"0px 10px"}}>
-            <div className="flex-accordion">
-            <h3 style={{color: "red"}}>Vadodara Etymology</h3>
-            <button className="accordion-button" onClick={()=>showPanel(1)}>{(activePanel===1)?"-":"+"}</button>
-            </div>
-            {(activePanel===1) && <Panel activePanel={activePanel} />}
-            </div>
-            <div style={{border: "1px solid black", padding:"5px", margin:"0px 10px"}}>
-            <div className="flex-accordion">
-            <h3 style={{color: "blue"}}>Vadodara Geography</h3>
-            <button className="accordion-button" onClick={()=>showPanel(2)}>{(activePanel===2)?"-":"+"}</button>
-             </div>
-             {(activePanel===2) && <Panel activePanel={activePanel} />}
-             </div>
-             <div style={{border: "1px solid black", padding:"5px", margin:"0px 10px"}}>
-            <div className="flex-accordion">
-            <h3 style={{color: "green"}}>Vadodara Climate</h3>
-            <button className="accordion-button" onClick={()=>showPanel(3)}>{(activePanel===3)?"-":"+"}</button>
-             </div>
-             {(activePanel===3) && <Panel activePanel={activePanel} />}
 
-             </div>
            
-            
+            {panelData.map((index) => {
+                return <p key={index}>{index.value}</p>
+            })
 
+            }
+            {/*<Panel showPanel={showPanel}/>*/}
              </>);
           
 
