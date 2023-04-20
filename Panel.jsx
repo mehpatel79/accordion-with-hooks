bc3123fd7e0e9e0cb7886198301d242171c0095f
mehpatel79 <123248785+mehpatel79@users.noreply.github.com>
2023-04-20 18:57:51 +0530

    Stage 3 - Work in progress

--- Panel.jsx
+++ Panel.jsx
@@ -1,22 +1,17 @@
 import React, { useState } from "react";
 import './flex-accordion.css';
 
-const Panel = ({panelData, showPanel, activePanel}) =>{
+const Panel = ({panelTitle, panelContent, showPanel, activePanel, index}) =>{
     
-    return(<>    
-            {panelData.map((data, index) => {
-                return (<>                
-                {console.log(index)}
-                        <div className="panel_display" key={index}>
-                            <div className="flex-accordion">
-                                <h3 className="h3_display">{data.title}</h3>
-                                <button className="accordion-button" onClick={()=>{showPanel(index)}}>{(activePanel===index)?"-":"+"}</button>
-                            </div>
-                            {(activePanel===index)?<p>{data.content}</p>:null}
-                        </div>
-                        </>);     
-                }
-            )}
+    return(<>
+            <div className="panel_display" >
+            <div className="flex-accordion">
+                <h3 className="h3_display">{panelTitle}</h3>
+                <button className="accordion-button" onClick={()=>{showPanel(index)}}>{(activePanel===index)?"-":"+"}</button>
+            </div>                
+                        
+            <p>{panelContent}</p>
+            </div>    
         </>);
     }
 export default Panel;
